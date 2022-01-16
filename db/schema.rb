ed0019fc87a49d26683ea9188840aaf057296024
Luke Cassar <luke@tanda.co>
2022-01-16 12:05:25 +1000

    Add not null validations to user and email

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_16_013423) do
+ActiveRecord::Schema.define(version: 2022_01_16_015304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "stripe_customer_id"
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name cannot be nil" do
+    error = assert_raises ActiveRecord::RecordInvalid do
+      User.new(name: "", email: "rudiger@hey.com").save!
+    end
+    assert_equal "Validation failed: Name can't be blank", error.message
+  end
+
+  test "email cannot be blank" do
+    error = assert_raises ActiveRecord::RecordInvalid do
+      User.new(name: "Rudiger", email: "").save!
+    end
+    assert_equal "Validation failed: Email can't be blank", error.message
+  end
 end
